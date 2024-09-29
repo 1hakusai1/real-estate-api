6ed12812a5bbfe012db07e7841e4568ef6ca43d2
1hakusai1 <1hakusai1@gmail.com>
2024-09-29 18:15:50 +0900

    zodでリクエストにバリデーションをかける

---
+++ src/estate-transaction/GetEstateTransactionInfoRequest.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+export const GetEstateTransactionInfoRequest = z.object({
+  year: z.coerce.number().min(2009).max(2021),
+  prefCode: z.coerce.number().min(1).max(47),
+  cityCode: z.union([z.string().regex(/[0-9]+/), z.literal('-')]),
+  displayType: z.union([
+    z.literal('1'),
+    z.literal('2'),
+    z.literal('3'),
+    z.literal('4'),
+    z.literal('5'),
+  ]),
+});
--- src/estate-transaction/estate-transaction.controller.ts
+++ src/estate-transaction/estate-transaction.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, HttpException, Query } from '@nestjs/common';
 import { EstateTransactionUsecase } from 'src/estate-transaction/estate-transaction.usecase';
+import { GetEstateTransactionInfoRequest } from 'src/estate-transaction/GetEstateTransactionInfoRequest';
 
 @Controller('api/v1/townPlanning/estateTransaction/bar')
 export class EstateTransactionController {
   constructor(private usecase: EstateTransactionUsecase) {}
   @Get()
   getEstateTransactionInfo(@Query() params: unknown) {
-    console.log(params);
+    const parsed = GetEstateTransactionInfoRequest.safeParse(params);
+    if (!parsed.success) throw new HttpException('BAD_REQUEST', 400);
+
     return { message: this.usecase.getEstateTransactionInfo() };
   }
 }
