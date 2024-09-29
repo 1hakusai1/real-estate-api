f8594fb80c26c882c275b3b81ec1d9a45cb24832
1hakusai1 <1hakusai1@gmail.com>
2024-09-29 17:30:13 +0900

    クエリパラメータを受け取れることを確認

--- src/estate-transaction/estate-transaction.controller.ts
+++ src/estate-transaction/estate-transaction.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { EstateTransactionUsecase } from 'src/estate-transaction/estate-transaction.usecase';
 
 @Controller('api/v1/townPlanning/estateTransaction/bar')
 export class EstateTransactionController {
   constructor(private usecase: EstateTransactionUsecase) {}
   @Get()
-  getEstateTransactionInfo() {
+  getEstateTransactionInfo(@Query() params: unknown) {
+    console.log(params);
     return { message: this.usecase.getEstateTransactionInfo() };
   }
 }
