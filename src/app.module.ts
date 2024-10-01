fdb2a2328d2c6d87622b92ec78d2f1ccfb63945a
1hakusai1 <1hakusai1@gmail.com>
2024-10-01 13:37:02 +0900

    不要なコードを削除
    最初に生成されたコードたち

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EstateTransactionModule } from './estate-transaction/estate-transaction.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ConfigModule.forRoot(), EstateTransactionModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
